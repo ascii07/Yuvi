47826be41a237bc4ba8a3eb22359d6c8c1bd5ab8
Rahul Singh <rrrsingh58@gmail.com>
2024-06-19 18:48:27 +0530

    Create Fantasy.php

---
+++ src/Fantasy.php
@@ -0,0 +1,135 @@
+<!doctype html>
+<html lang="en">
+
+
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title></title>
+    <style>
+        body {
+            font-family: "Source Sans Pro", sans-serif;
+            position: relative;
+            
+            margin: 0;
+            padding: 0;
+        }
+
+        .container,
+        .container-md,
+        .container-sm {
+            max-width: 900px;
+        }
+
+        .ads-col {
+            padding: 20px 0px;
+        }
+
+        .img-fluid {
+            width: 100%;
+            height: auto;
+        }
+    </style>
+    <style>
+                .footer a {
+                    color: #fab800;
+                    font-weight: 700;
+                }
+            @media only screen and (max-width: 480px) {
+                .footer {
+                   position: fixed;
+                   left: 0;
+                   bottom: 0;
+                   width: 100%;
+                   background-color: #000;
+                   text-align: center;
+                    z-index: 9999;
+                }
+            }
+        @media (min-width: 480px) and (max-width: 767px)
+            {
+               .new
+               {
+                  width: 100% !important;
+                  height: 100% !important; 
+                }
+                
+            }
+            @media (min-width: 768px)
+            {
+                .modal-dialog {
+                    width: 280px !important;
+                }
+            }
+
+
+
+
+
+
+#social_side_links {
+    position: fixed;
+      bottom: 67px;
+   
+  padding:0px;
+  list-style: none;
+  z-index: 99;
+}
+
+#social_side_links li a {display: block;}
+
+#social_side_links li a img {
+    display: block;
+    max-width:100%;
+  padding:0px;
+  -webkit-transition:  background .2s ease-in-out;
+  -moz-transition:  background .2s ease-in-out;
+  -o-transition:  background .2s ease-in-out;
+  transition:  background .2s ease-in-out;
+}
+
+#social_side_links li a:hover img {background: rgba(0, 0, 0, .2);}
+            
+    </style>
+
+</head>
+
+<body>
+ <ul id="social_side_links">
+    <li><a target="_blank" href="https://wa.link/search2"><img src="rahul.gif" style="width:90px;height:90px; " border="0" alt="" class="img-responsive"></a> 
+
+    </li>
+</ul>
+
+
+
+
+
+
+
+    <div class="ads-campion">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <div class="ads-col text-center">
+                        <a target="__blank" href="https://wa.link/search2 ">
+                            <img src="" class="img-fluid" alt="" >
+                        </a>
+                        <!-- <p>Disclaimer:- This Website is only for 18+ users. Please Leave This Site If you are from Telangana, Orissa, Assam, Sikkim, and Nagaland. Be aware of fraudsters, we only deal via WhatsApp. No Real Money Involvement</p> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- <div class="footer">
+      <p style="margin-top: 10px;font-size: 35px;text-transform: uppercase;"><a target="__blank" href="https://wa.link/search2" >Click Here</a></p>
+    </div> -->
+    </body>
+      <!-- <p>18+ Only</p>   -->
+    
+
+
+</html>
